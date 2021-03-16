506c26bad7f76c0559163d34e37727e2b7a8b135
indranil9 <46261718+indranil9@users.noreply.github.com>
2021-03-16 18:45:06 +0530

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,6 @@
+create table studentData (Name TEXT,Rollno INT NOT NULL PRIMARY KEY,Address TEXT,MobileNumber TEXT,PanNumber TEXT);
+insert into studentData (Name,Rollno,Address,MobileNumber,PanNumber) values ('Indranil Das','100','Kolkata','+918912547845','E145616161');	
+insert into studentData (Name,Rollno,Address,MobileNumber,PanNumber) values ('Debargha Ghosh','101','Chennai','+914582547845','Q145616161');
+insert into studentData (Name,Rollno,Address,MobileNumber,PanNumber) values ('Kumar Aman','102','Delhi','+914582547845','M145616161');
+insert into studentData (Name,Rollno,Address,MobileNumber,PanNumber) values ('Anurag Pundir','103','Pune','+914582547845','U145616161');
+insert into studentData (Name,Rollno,Address,MobileNumber,PanNumber) values ('Akash Das','104','Bangalore','+917892547845','X145616161');	
